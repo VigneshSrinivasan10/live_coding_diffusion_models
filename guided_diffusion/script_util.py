229c5ba16a0f0913448e63c68e9b2ed62e98bcef
VigneshSrinivasan10 <vignesh.mssrinivasan@gmail.com>
2023-02-02 11:00:18 +0100

    cifar channels 3

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -169,7 +169,7 @@ def create_model(
         image_size=image_size,
         in_channels=1,
         model_channels=num_channels,
-        out_channels=(1 if not learn_sigma else 2),
+        out_channels=(3 if not learn_sigma else 2),
         num_res_blocks=num_res_blocks,
         attention_resolutions=tuple(attention_ds),
         dropout=dropout,
